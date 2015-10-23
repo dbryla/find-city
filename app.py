36db74aa58704a377b65e1db369cee867c014d61
Daniel Bryla <dbryla@student.agh.edu.pl>
2015-10-24 00:01:46 +0200

    Initial tornado commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+from tornado import web, ioloop
+
+settings = {
+    "template_path": os.path.join(os.path.dirname(__file__), "templates"),
+    "static_path": os.path.join(os.path.dirname(__file__), "static"),
+    "debug": True
+}
+
+class IndexHandler(web.RequestHandler):
+    def get(self):
+        self.render("index.html")
+
+application = web.Application([
+    (r'/', IndexHandler),
+    (r'/index', IndexHandler),
+], **settings)
+
+port = 8080
+
+if __name__ == '__main__':
+    application.listen(port)
+    ioloop.IOLoop.instance().start()
