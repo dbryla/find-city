9d5f958265405306a152bfab1c62213a6b480061
Dziunieeek <jarek822@gmail.com>
2015-10-24 00:53:41 +0200

    Cudowny socket

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from tornado import web, ioloop
+from tornado import web, ioloop, websocket
 
 settings = {
     "template_path": os.path.join(os.path.dirname(__file__), "templates"),
@@ -8,13 +8,29 @@ settings = {
     "autoreload": True
 }
 
+
+class SocketHandler(websocket.WebSocketHandler):
+    def open(self):
+        print 'connection opened...'
+        self.write_message("The server says: 'Hello'. Connection was accepted.")
+
+    def on_message(self, message):
+        self.write_message("The server says: " + message + " back at you")
+        print 'received:', message
+
+    def on_close(self):
+        print 'connection closed...'
+
+
 class IndexHandler(web.RequestHandler):
     def get(self):
         self.render("index.html")
 
+
 application = web.Application([
     (r'/', IndexHandler),
     (r'/index', IndexHandler),
+    (r'/socket', SocketHandler)
 ], **settings)
 
 port = 8080
