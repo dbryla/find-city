a95140436ccf2b5ff6bb2c3cc36fe6c6a93e4954
Maciej Warchoł <maciewar@gmail.com>
2015-10-24 23:44:23 +0200

    Add info about no records

--- static/js/rank.js
+++ static/js/rank.js
@@ -2,12 +2,16 @@ function populateRankTable(rank) {
     var table = $("#rank-table");
     table.empty();
     var row, name, score;
-    for (var i = 0; i < rank.length; i++) {
-        row = rank[i];
-        name = row[0];
-        score = row[1];
-        table.append("<tr><td>" + (i + 1) + ".</td><td>" + name + "</td><td>" + score + "</td></tr>")
-        table.append("<tr><td>" + (i + 1) + ".</td><td>" + name + "</td><td>" + score + "</td></tr>")
+    if (rank.length == 0) {
+        $("#no-records").show();
+    } else {
+        $("#no-records").hide();
+        for (var i = 0; i < rank.length; i++) {
+            row = rank[i];
+            name = row[0];
+            score = row[1];
+            table.append("<tr><td>" + (i + 1) + ".</td><td>" + name + "</td><td>" + score + "</td></tr>")
+        }
     }
 }
 
@@ -15,7 +19,7 @@ var id;
 var send;
 
 function getRank() {
-    $.get( "/get", function( data ) {
+    $.get("/get", function (data) {
         populateRankTable(data["rank"]);
     });
 }
