fe46ea7a066a5e1b73bf44b86812d2c078a4b427
Dziunieeek <jarek822@gmail.com>
2015-10-24 02:56:03 +0200

    Sockets with id

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import random
 from tornado import web, ioloop, websocket
 
 settings = {
@@ -8,18 +9,29 @@ settings = {
     "autoreload": True
 }
 
+sockets = {}
+
 
 class SocketHandler(websocket.WebSocketHandler):
+
     def open(self):
-        print 'connection opened...'
-        self.write_message("The server says: 'Hello'. Connection was accepted.")
+        id = self.get_random()
+        sockets[id] = self
+        print 'connection opened with id =', id
+        self.write_message({"id": id})
 
     def on_message(self, message):
-        self.write_message("The server says: " + message + " back at you")
+
+        for x, y in sockets.items():
+            y.write_message({"msg": message})
+
         print 'received:', message
 
     def on_close(self):
         print 'connection closed...'
+
+    def get_random(self):
+        return random.randint(1000000, 9999999)
 
 
 class IndexHandler(web.RequestHandler):
