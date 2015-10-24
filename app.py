9e3f086f20e9fadc0eb8214ff58bdf912b428f8d
Daniel Bryla <dbryla@student.agh.edu.pl>
2015-10-24 08:49:17 +0200

    Moved handlers to another file

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import os
-import json
 
-from tornado import web, ioloop, websocket
+from tornado import web, ioloop
 from db import createDB
+from handlers import IndexHandler, SocketHandler, FriendHandler
 
-import msg
-from game import Player, Game, PlayerClick
-from utils import getRandom
 
 settings = {
     "template_path": os.path.join(os.path.dirname(__file__), "templates"),
@@ -14,81 +11,6 @@ settings = {
     "debug": True,
     "autoreload": True
 }
-
-players = {}
-free_players = []
-
-ACTION_FIELD = 'action'
-PLAY_ACTION = 'play'
-ID = 'id'
-X = 'x'
-Y = 'y'
-TIME = 'time'
-MSG = 'msg'
-
-class SocketHandler(websocket.WebSocketHandler):
-
-    def open(self):
-        id = getRandom()
-        player = Player(id, self)
-        players[id] = player
-        print 'connection opened with id =', id
-
-        self.write_message(msg.init(id))
-
-        if len(free_players) != 0:
-            # match waiting player
-            another_player = free_players.pop()
-            Game(player, another_player).start()
-        else:
-            # wait for another player
-            free_players.append(player)
-
-
-
-    def on_message(self, message):
-        message = json.loads(message)
-        try:
-            message[ID]
-        except:
-            raise web.HTTPError(400, "ERROR: No id.")
-
-        if message[ACTION_FIELD] == PLAY_ACTION:
-            players[message[ID]].endGame(PlayerClick(message[X], message[Y], message[TIME]))
-
-
-    def on_close(self):
-        print 'connection closed...'
-
-class FriendHandler(websocket.WebSocketHandler):
-
-    def open(self):
-        id = getRandom()
-        self.player = Player(id, self)
-        players[id] = self.player
-        self.write_message(msg.init(id))
-
-    def on_message(self, message):
-        message = json.loads(message)
-        try:
-            message[ID]
-        except:
-            raise web.HTTPError(400, "ERROR: No id.")
-
-        print players
-        if message[ACTION_FIELD] == 'friend':
-            Game(self.player, players[message[MSG]]).start()
-
-        if message[ACTION_FIELD] == PLAY_ACTION:
-            players[message[ID]].endGame(PlayerClick(message[X], message[Y], message[TIME]))
-
-    def on_close(self):
-        print 'connection closed...'
-
-class IndexHandler(web.RequestHandler):
-    def get(self):
-        self.render("index.html")
-
 
 application = web.Application([
     (r'/', IndexHandler),
---
+++ handlers.py
@@ -0,0 +1,79 @@
+import json
+from tornado import websocket, web
+from game import Player, Game, PlayerClick
+import msg
+from utils import getRandom
+
+players = {}
+free_players = []
+
+ACTION_FIELD = 'action'
+PLAY_ACTION = 'play'
+ID = 'id'
+X = 'x'
+Y = 'y'
+TIME = 'time'
+MSG = 'msg'
+
+class SocketHandler(websocket.WebSocketHandler):
+
+    def open(self):
+        id = getRandom()
+        player = Player(id, self)
+        players[id] = player
+        print 'connection opened with id =', id
+
+        self.write_message(msg.init(id))
+
+        if len(free_players) != 0:
+            # match waiting player
+            another_player = free_players.pop()
+            Game(player, another_player).start()
+        else:
+            # wait for another player
+            free_players.append(player)
+
+
+
+    def on_message(self, message):
+        message = json.loads(message)
+        try:
+            message[ID]
+        except:
+            raise web.HTTPError(400, "ERROR: No id.")
+
+        if message[ACTION_FIELD] == PLAY_ACTION:
+            players[message[ID]].endGame(PlayerClick(message[X], message[Y], message[TIME]))
+
+
+    def on_close(self):
+        print 'connection closed...'
+
+class FriendHandler(websocket.WebSocketHandler):
+
+    def open(self):
+        id = getRandom()
+        self.player = Player(id, self)
+        players[id] = self.player
+        self.write_message(msg.init(id))
+
+    def on_message(self, message):
+        message = json.loads(message)
+        try:
+            message[ID]
+        except:
+            raise web.HTTPError(400, "ERROR: No id.")
+
+        print players
+        if message[ACTION_FIELD] == 'friend':
+            Game(self.player, players[message[MSG]]).start()
+
+        if message[ACTION_FIELD] == PLAY_ACTION:
+            players[message[ID]].endGame(PlayerClick(message[X], message[Y], message[TIME]))
+
+    def on_close(self):
+        print 'connection closed...'
+
+class IndexHandler(web.RequestHandler):
+    def get(self):
+        self.render("index.html")
