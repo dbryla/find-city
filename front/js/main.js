651ee6389e3a10073e8e7498140273efa730cd66
Maciej Warchoł <maciewar@gmail.com>
2015-10-24 06:15:55 +0200

    Add top panels in map view

--- front/js/main.js
+++ front/js/main.js
@@ -1,6 +1,54 @@
-$(document).ready(function () {
+var timerInterval = 0;
+
+function setScore(value) {
+    $("#score").text("Score: " + value);
+}
+
+function decrementTimer() {
+    var curTime = getTimerValue();
+    if (curTime > 0) {
+        $("#timer-value").text(curTime - 1);
+    } else {
+        clearInterval(timerInterval);
+    }
+}
+
+function getTimerValue() {
+    var timerText = $("#timer").text();
+    var timeString = timerText.split(":")[1];
+    return parseInt(timeString);
+}
+
+function startTimer(value, text) {
+    clearInterval(timerInterval);
+    timerInterval = setInterval(decrementTimer, 1000);
+    $("#timer-value").text(value);
+    $("#timer-text").text(text);
+}
+
+function ask_question(country_code, city) {
+    var countryStr = countries[country_code.toUpperCase()];
+    $("#country").text(countryStr);
+    $("#flag").attr("src", "img/flags/" + country_code.toLowerCase() + ".png");
+    $("#city").text(city);
+}
+
+function setGameScene() {
+    $("#welcome").hide();
+    $("#game-wrapper").show();
+}
+
+function init() {
     $("#play-btn").click(function () {
-        $("#welcome").hide();
-        $("#game").show();
+        setGameScene();
+        ask_question("de", "Berlin");
+        startTimer(5, "Next country in: ");
+        setScore(12120);
     });
+}
+
+$(document).ready(function () {
+    init();
+    //for debugging purpose only
+    //$("#play-btn").click();
 });
