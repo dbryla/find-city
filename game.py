f5876c0c48f2a417cb6b27d75aa7a463e1dc50fb
Daniel Bryla <dbryla@student.agh.edu.pl>
2015-10-24 15:34:15 +0200

    Added timeouts

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from threading import Timer
 from msg import gameStart, gameEnd, gameWait
 from utils import generateCity, distance
 import time
@@ -5,6 +6,7 @@ import time
 
 class Game(object):
     completed = False
+    timeout = True
 
     def __init__(self, player1, player2):
         self.round_number = 1
@@ -20,36 +22,71 @@ class Game(object):
         self.sendToPlayers(wait)
         time.sleep(5)
         self.sendToPlayers(start)
+        Timer(15.0, self.timeout).start()
+
+    def timeout(self):
+        if self.timeout:
+            self.end()
 
     def end(self):
         if self.completed:
             game_end_message = gameEnd(self.chooseWinner())
+            self.timeout = False
             self.round_number += 1
             self.player1.click = None
             self.player2.click = None
             self.sendToPlayers(game_end_message)
             if self.round_number <= 10:
+                self.timeout = True
                 self.start()
         else:
             self.completed = True
 
+    def returnPlayerData(self, player):
+        player_x = player.click.x
+        player_y = player.click.y
+        return ({"x": player_x, "y": player_y, "time": player.click.time},
+                distance(player_x, player_y, self.city.x, self.city.y))
+
     def chooseWinner(self):
-        player1 = {"x": self.player1.click.x, "y": self.player1.click.y, "time": self.player1.click.time}
-        player2 = {"x": self.player2.click.x, "y": self.player2.click.y, "time": self.player2.click.time}
-
-        dist1 = distance(player1["x"], player1["y"], self.city.x, self.city.y)
-        dist2 = distance(player2["x"], player2["y"], self.city.x, self.city.y)
-
-        result1 = dist1 + player1["time"]
-        result2 = dist2 + player2["time"]
-
-        return {"players":
-                        [
-                            {"id": self.player1.id, "win": result1 < result2, "dist": dist1, "point": 10000 / dist1, "click": player1, "result1": self.game.result1},
-                            {"id": self.player2.id, "win": result1 > result2, "dist": dist2, "point": 10000 / dist1, "click": player2, "player1": self.game.result1 }
-                        ],
+        if self.timeout:
+            if not self.player1.click and not self.player2.click:
+                return {}
+            elif self.player2.click:
+                player2, dist2 = self.returnPlayerData(self.player2)
+                return {"players":
+                    [
+                        {"id": self.player1.id, "win": False, "point": 0},
+                        {"id": self.player2.id, "win": True, "dist": dist2, "point": 10000 / dist2,
+                         "click": player2}
+                    ],
                     "location": {"x": self.city.x, "y": self.city.y}
                 }
+            else:
+                player1, dist1 = self.returnPlayerData(self.player1)
+                return {"players":
+                    [
+                        {"id": self.player1.id, "win": True, "dist": dist1, "point": 10000 / dist1,
+                         "click": player1},
+                        {"id": self.player2.id, "win": False, "point": 0},
+                    ],
+                    "location": {"x": self.city.x, "y": self.city.y}
+                }
+        else:
+            player1, dist1 = self.returnPlayerData(self.player1)
+            player2, dist2 = self.returnPlayerData(self.player2)
+            result1 = dist1 + player1["time"]
+            result2 = dist2 + player2["time"]
+
+        return {"players":
+            [
+                {"id": self.player1.id, "win": result1 < result2, "dist": dist1, "point": 10000 / dist1,
+                 "click": player1},
+                {"id": self.player2.id, "win": result1 > result2, "dist": dist2, "point": 10000 / dist2,
+                 "click": player2}
+            ],
+            "location": {"x": self.city.x, "y": self.city.y}
+        }
 
     def rageQuit(self, id):
         if id != self.player1.id:
